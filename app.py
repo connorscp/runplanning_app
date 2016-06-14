6b9581cb7c55f2c8afef3e5313b5a33019a72f54
Colin <connorscp@gmail.com>
2016-06-14 17:54:50 -0400

    more groundup

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, session
 import requests
 import simplejson
-from stravalib import Client
+import stravalib
 import os
 
 app = Flask(__name__)

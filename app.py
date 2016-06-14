d1e4677cfbd866c4eb58df287ade984f5bf0cec2
Colin <connorscp@gmail.com>
2016-06-14 17:23:03 -0400

    small change

--- app.py
+++ app.py
@@ -20,12 +20,13 @@ def homepage():
 @app.route('/authenticate',methods=['GET','POST'])
 def authenticate():
 	if request.method =='GET':
-		client = stravalib.client.Client() #
-		auth_url = client.authorization_url(client_id=client_id, #
-			scope='view_private', #
-			redirect_uri='https://strava-test.herokuapp.com/authorized') #
-		print auth_url
-		return render_template('authenticate.html', auth_url=auth_url)
+		#client = stravalib.client.Client() #
+		#auth_url = client.authorization_url(client_id=client_id, #
+		#	scope='view_private', #
+		#	redirect_uri='https://strava-test.herokuapp.com/authorized') #
+		#print auth_url
+		#return render_template('authenticate.html', auth_url=auth_url)
+		return render_template('authenticate.html')
 
 	else:
 		# request was a POST

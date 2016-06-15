9f8217097d1b1553d597f3aa4beeaabaf8fd7f79
Colin <connorscp@gmail.com>
2016-06-15 13:14:46 -0400

    store user_tokens in json through app

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, session
 import requests
-import simplejson
+import json
 import stravalib
 import os
 
@@ -23,15 +23,6 @@ def authenticate():
 	# if request.method =='GET':
 	return render_template('authenticate.html')
 
-	# else:
-	#	# request was a POST
-	#	code = request.args.get('code', code)
-	#	print "code cc: " + str(code)
-	#	access_token = client.exchange_code_for_token(client_id=client_id, client_secret=str(client_secret), code=code)
-	#	print "access token cc: " + str(access_token)
-
-	#	return render_template('authorization.html')
-
 @app.route('/authorized', methods=['GET', 'POST'])
 def authorized():
 	code = request.args.get('code')
@@ -40,38 +31,22 @@ def authorized():
 		client_secret=client_secret,
 		code=code)
 
-	print token
+	# Write user's username and token to tokens.json
+	user = stravalib.Client(token)
+	athlete = user.get_athlete()
 
-	client2 = stravalib.Client(token)
-	athlete = client2.get_athlete()
+	username = athlete.username
+
+	user_tokens[username] = token
+
+	with open('tokens/tokens.json', 'w') as f:
+		json.dump(user_tokens, f)
+
+	# Return authorized page
 	name = athlete.firstname
-	email = athlete.email
-
-	print name
-	print email
-
 
 	return render_template('authorized.html', code=code, token=token, name=name, email=email)
-"""
-Congratulations! We've successfully authorized to connect your running history to 
-the Run Planning app! The code is 6546d8346384d41dc26a99f5d40654981355d415. 
-The token is e4bd0abdb7726b04a7a2c715684b0576f14414ca
-"""
 
-
-
-
-"""
-@app.route('/authorized')
-def authorized():
-	code = flask.request.args.get('code', '')
-    client = stravalib.client.Client()
-    token = client.exchange_code_for_token(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, code = code)
-    flask.session['access_token'] = token
-    print token
-    # return flask.redirect(flask.url_for('homepage'))
-    return render_template('authorized.html')
-"""
 
 if __name__ == '__main__':
 	# app.run(host='0.0.0.0') # when running on DO. Start w/ vagrant:5000/index
